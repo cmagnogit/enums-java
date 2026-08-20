3461fa5396f03c468f10b5715019983457ba9da8
cmagno <corellanc@gmail.com>
2026-08-20 10:18:39 -0500

    Functional code in our enums application

--- src/TestContinent.java
+++ src/TestContinent.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class testContinent {
     public static void main(String[] args) {
-        Continent[] continents = Continent.values();
-        for(Continent c:continents)
-            System.out.println(c.name() + " ->" + c + ", population density= "
-                    + c.populationDensity() + "}");
+        List<Continent> continents = new ArrayList<>(Arrays.asList(Continent.values()));
+        continents.forEach(c -> System.out.println(c.name() + " ->" + c + ", population density= " +
+                c.populationDensity() + "}"));
     }
 }
